00f59c5567b74c37ec37eb0ffd904b74620f2120
NeishchenkoAlex <br.cheg@mail.ru>
2025-02-03 00:20:57 +0300

    step 1

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,3 +1,10 @@
-def hello():
-    print('Welcome to the Brain Games!')
+def main():
+    print("Welcome to the Brain Games!")
+
+
+if __name__ == "__main__":
+    main()
+
+
+
 
--- brain_games/scripts/main.py
+++
@@ -1,5 +0,0 @@
-from brain_games.scripts.brain_games import hello
-def main():
-    hello()
-if __name__=='__main__':
-    main()
