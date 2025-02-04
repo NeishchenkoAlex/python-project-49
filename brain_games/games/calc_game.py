82ff4e662e388c3522fe2fe1ab4a015ef6c3cf30
NeishchenkoAlex <br.cheg@mail.ru>
2025-02-05 00:04:53 +0300

    fixed error,touch func,w_u,

--- brain_games/games/calc_game.py
+++ brain_games/games/calc_game.py
@@ -1,10 +1,26 @@
+from random import choice, randint
+
 from brain_games.games.logic_calc import game
+
 
 def calc_game():
 	
     def rule():
         print('What is the result of the expression?')
 
+    def query():
+        random_choice = (choice([' + ', ' * ', ' - ']))
+        n1 = randint(1, 100)
+        n2 = randint(1, 100)
+        
+        question1 = str(n1) + random_choice + str(n2)
+        
+        if random_choice == ' + ':
+            correct_answer = n1 + n2
+        if random_choice == ' - ':
+            correct_answer = n1 - n2
+        if random_choice == ' * ':
+            correct_answer = n1 * n2
+        return question1, correct_answer
 
-	
-    game(rule)
+    game(rule, query)
--- brain_games/games/logic_calc.py
+++ brain_games/games/logic_calc.py
@@ -1,25 +1,13 @@
-from random import randint , choice
-def game(rule):
-    print('Welcome to the Brain Games!')
-    name = input('May I have your name? ')
-    print(f'Hello, {name}!')
-    ROUNDS = 3
+from brain_games.games.brain_welcome import ROUNDS, name
+from brain_games.games.calc_game import correct_answer, question1
+
+
+def game(rule, query, welcome_user):
+    welcome_user()
     rule()
     
     for _ in range(ROUNDS):
-        random_choice = (choice([' + ', ' * ', ' - ']))
-        n1 = randint(1,100)
-        n2 = randint(1,100)
-        question1 = str(n1)+random_choice+str(n2)
-        if random_choice == ' + ':
-            correct_answer=n1+n2
-        if random_choice == ' - ':
-            correct_answer=n1-n2
-        if random_choice == ' * ':
-            correct_answer=n1*n2
         
-        
-
         question = question1
 
         print(f'Question: {question}')
@@ -29,7 +17,8 @@ def game(rule):
         else:
             print(f"Question: {question}\n"
                   f"Your answer: {answer}.\n"
-                  f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.\n"
+                  f"""'{answer}' is wrong answer ;(. 
+                  Correct answer was '{correct_answer}'.\n"""
                   f"Let's try again, {name}!")
             break
     else:
